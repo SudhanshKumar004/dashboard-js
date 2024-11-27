b2fd861f3ddf12a448220185d22ba1a8abd4e5ec
Sudhansh Kumar <sudhanshkb04@gmail.com>
2024-11-27 23:14:58 +0530

    created structure for value input

---
+++ login.js
@@ -0,0 +1,12 @@
+function loginpage(){
+    let getemail = localStorage.getItem("email");
+    let getpsw = localStorage.getItem("password");
+    let email = document.querySelector('#email1').value;
+    let psw = document.querySelector('#psw2').value;
+
+    if(getemail == email && getpsw == psw){
+        window.alert("done")
+        window.location.href = "./dash.html"
+    }
+
+}
---
+++ signup.js
@@ -0,0 +1,69 @@
+function data(){
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let number = document.querySelector('#number').value;
+    let psw = document.querySelector('#psw').value;
+
+
+    if(name === ""){
+        document.querySelector('#errorname').innerHTML = "Please enter name";
+        let selectedname = document.querySelector('#name');
+        selectedname.style.borderColor = "red";
+        selectedname.style.outlineColor = "red";
+    }
+
+    if(email === ""){
+        document.querySelector('#erroremail').innerHTML = "Please enter email"
+        let selectedemail = document.querySelector('#email');
+        selectedemail.style.borderColor = "red";
+        selectedemail.style.outlineColor = "red";
+    }
+
+    else if(!(email.includes('@') && email.includes('.com'))){
+        document.querySelector('#erroremail').innerHTML = "Enter valid email.";
+        let selectemail = document.querySelector('#email');
+        selectemail.style.borderColor = "red";
+        selectemail.style.outlineColor = "red";
+    }
+
+    if(number === ""){
+        document.querySelector('#errornum').innerHTML = "Please enter number"
+        let selectednumber = document.querySelector('#number');
+        selectednumber.style.borderColor = "red";
+        selectednumber.style.outlineColor = "red";
+    }
+
+     else if(number.length !== 10){
+        document.querySelector('#errornum').innerHTML = "Please enter 10 digit number";
+        let selectnumber = document.querySelector('#number');
+        selectnumber.style.borderColor = "red";
+        selectnumber.style.outlineColor = "red";
+    }
+
+     else if(isNaN(number)){
+        document.querySelector('#errornum').innerHTML = "Please enter numbers only";
+        let selectnumber = document.querySelector('#number');
+        selectnumber.style.borderColor = "red";
+        selectnumber.style.outlineColor = "red";
+    }
+    
+    if(psw === ""){
+        document.querySelector('#errorpsw').innerHTML = "Please enter password"
+        let selectedpsw = document.querySelector('#psw');
+        selectedpsw.style.borderColor = "red";
+        selectedpsw.style.outlineColor = "red";
+        return false;
+    }
+
+     else if(!(psw.match(/[123456789]/) && psw.match(/[!@#$%^&*()]/) && psw.match(/[qwertyuiopasdfghjklzxcvbnm]/) && (/[QWERTYUIOPASDFGHJKLZXCVBNM]/))){
+         document.querySelector('#errorpsw').innerHTML = "Password should contain upper case, lower case, spacial character, numbers";
+        let selectpsw = document.querySelector('#psw');
+        selectpsw.style.borderColor = "red";
+        selectpsw.style.outlineColor = "red";
+    }
+
+    localStorage.setItem("name", name)
+    localStorage.setItem("email", email)
+    localStorage.setItem("number", number)
+    localStorage.setItem("password", psw)
+}
